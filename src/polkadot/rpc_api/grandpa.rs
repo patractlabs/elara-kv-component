2b055e8d1126345013ae4cddba118ce818d7c316
yjhmelody <465402634@qq.com>
2021-02-06 17:32:08 +0800

    fix: fix grandpa api

--- src/polkadot/rpc_api/grandpa.rs
+++ src/polkadot/rpc_api/grandpa.rs
@@ -1,17 +1,16 @@
 use serde::{Deserialize, Serialize};
 
-/// storage data as Subscribed data in `result`
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(transparent)]
-pub struct GrandpaJustification(Vec<u8>);
+pub struct GrandpaJustification(String);
 
-impl From<Vec<u8>> for GrandpaJustification {
-    fn from(bytes: Vec<u8>) -> Self {
+impl From<String> for GrandpaJustification {
+    fn from(bytes: String) -> Self {
         Self(bytes)
     }
 }
 
-impl From<GrandpaJustification> for Vec<u8> {
+impl From<GrandpaJustification> for String {
     fn from(g: GrandpaJustification) -> Self {
         g.0
     }
