ea82501d1346bf8b0e32cd08f4801f4898e0b0a1
yjh <465402634@qq.com>
2021-09-28 11:56:34 +0800

    fix: fix blocking timeout and add more related config (#61)
    * fix: fix blocking timeout and more related config; decrease lock time for health check
    
    * fix test
    
    * fmt

--- src/config.rs
+++ src/config.rs
@@ -43,6 +43,7 @@ struct ServiceInnerConfig {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WsConfig {
     pub addr: String,
+    pub heartbeat_interval_sec: Option<u64>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -52,6 +53,7 @@ pub struct NodeConfig {
 
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub struct RpcClientConfig {
+    pub timeout_ms: Option<u64>,
     pub max_request_cap: Option<usize>,
     pub max_cap_per_subscription: Option<usize>,
 }
@@ -74,6 +76,7 @@ fn test_toml_config() {
     let config = ServiceInnerConfig {
         ws: WsConfig {
             addr: "localhost:9002".into(),
+            heartbeat_interval_sec: Some(10),
         },
         nodes,
         client: None,
